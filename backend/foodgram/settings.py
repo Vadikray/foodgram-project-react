11211b96366ee9f1b3d5890ca9430ffb87d41c06
Вадим Конюшков <konuskov92@gmail.com>
2023-03-14 01:31:03 +0500

    v2.0

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = (os.getenv('SECRET_KEY'),
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*', ]
+ALLOWED_HOSTS = ['51.250.103.190', '127.0.0.1', 'localhost', '*', ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
